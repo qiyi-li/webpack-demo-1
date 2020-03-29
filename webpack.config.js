45ac5c00744f49dad8d90f370a2ee4c4a0378328
a-lei-7 <qiyi.li@aol.com>
2020-03-29 16:26:28 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
   entry: "./src/index.js",
   output: {
     filename: "index.[contenthash].js" // 添加缓存
