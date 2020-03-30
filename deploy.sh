f305fafd4bd906c86f1d925639c74aae71103821
a-lei-7 <qiyi.li@aol.com>
2020-03-30 11:47:12 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@ yarn build &&
 git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
-rm -rf dist ;
+rm -rf dist;
 git add . &&
 git commit -m update &&
 git push &&
+git checkout -
